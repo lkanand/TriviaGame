298eff7885fae45cfc8751fa6c48f2cc0f399803
lokesh.k.anand@gmail.com <lokesh.k.anand@gmail.com>
2018-01-28 10:27:46 -0600

    Fully functioning game

---
+++ assets/javascript/app.js
@@ -0,0 +1,420 @@
+/*61TKqzUDPHfv40Bqr6iEsqqBCfa360mt*/
+
+var arrayOfQuestionObjects = [
+{"Question": "Edwin Jarvis serves as butler to what superteam?",
+"ChoiceOne": "Teen Titans", 
+"ChoiceTwo": "Fantastic Four", 
+"ChoiceThree": "Justice League", 
+"ChoiceFour": "Avengers", 
+"Answer": "Avengers",
+"GIPHYID": "Avengers"}, 
+
+{"Question": "Who was the first costumed superhero?",
+"ChoiceOne": "Arrow", 
+"ChoiceTwo": "Crimson Avenger", 
+"ChoiceThree": "Superman", 
+"ChoiceFour": "The Phantom", 
+"Answer": "The Phantom",
+"GIPHYID": "Marvel+Superhero"}, 
+
+{"Question": "Who was the first superhero Spider-Man ever encountered?",
+"ChoiceOne": "Terrible Tinkerer", 
+"ChoiceTwo": "Supercharger", 
+"ChoiceThree": "Doctor Octopus", 
+"ChoiceFour": "Green Goblin", 
+"Answer": "Supercharger",
+"GIPHYID": "Spider-Man"}, 
+
+{"Question": "What super-villain kidnapped and murdered Peter Parker's girlfriend Gwen Stacy?",
+"ChoiceOne": "The Rhino", 
+"ChoiceTwo": "Doctor Octopus", 
+"ChoiceThree": "Green Goblin", 
+"ChoiceFour": "Electro", 
+"Answer": "Green Goblin",
+"GIPHYID": "Green+Goblin"}, 
+
+{"Question": "What did Spider-Man bring home from the first 'Secret Wars'?",
+"ChoiceOne": "Alien Flu", 
+"ChoiceTwo": "A Keychain", 
+"ChoiceThree": "New Girlfriend", 
+"ChoiceFour": "New Costume", 
+"Answer": "New Costume", 
+"GIPHYID": "Spider-Man"},
+
+{"Question": "What was the name of Peter Parker's uncle?",
+"ChoiceOne": "Bruce", 
+"ChoiceTwo": "Ben", 
+"ChoiceThree": "Thomas", 
+"ChoiceFour": "Bob", 
+"Answer": "Ben", 
+"GIPHYID": "Uncle+Ben+Spider-Man"},
+
+{"Question": "Who was Peter Parker's first superhuman girlfriend?",
+"ChoiceOne": "Ms. Marvel", 
+"ChoiceTwo": "White Tiger", 
+"ChoiceThree": "Kitty Pryde", 
+"ChoiceFour": "Black Cat", 
+"Answer": "Black Cat", 
+"GIPHYID": "Spider-Man+Black+Cat"},
+
+{"Question": "What other superhero helped Spider-Man the first time he fought the Ringmaster and his Circus of Crime?",
+"ChoiceOne": "Iron Man", 
+"ChoiceTwo": "Human Torch", 
+"ChoiceThree": "Hawkeye", 
+"ChoiceFour": "Daredevil", 
+"Answer": "Daredevil", 
+"GIPHYID": "Marvel's+Daredevil"},
+
+{"Question": "Which Spider-Man villain dresses and acts like a 1920s gangster?",
+"ChoiceOne": "Mister Negative", 
+"ChoiceTwo": "Finisher", 
+"ChoiceThree": "Big Wheel", 
+"ChoiceFour": "Hammerhead", 
+"Answer": "Hammerhead", 
+"GIPHYID": "Al+Capone"},
+
+{"Question": "What is Clark Kent's middle name?",
+"ChoiceOne": "Joseph", 
+"ChoiceTwo": "Walter", 
+"ChoiceThree": "Thomas", 
+"ChoiceFour": "Jacob", 
+"Answer": "Joseph", 
+"GIPHYID": "Clark+Kent"},
+
+{"Question": "What is the name of the dimensional plane where Kryptonian criminals were exiled?",
+"ChoiceOne": "No Man's Land", 
+"ChoiceTwo": "Zimbardo's Prison", 
+"ChoiceThree": "Bizarro World", 
+"ChoiceFour": "Phantom Zone", 
+"Answer": "Phantom Zone", 
+"GIPHYID": "Phantom+Zone+Superman"},
+
+{"Question": "Which of Superman's sweethearts became the super-heroine known as Insect Queen?",
+"ChoiceOne": "Lana Lang", 
+"ChoiceTwo": "Lois Lane", 
+"ChoiceThree": "Lori Lemaris", 
+"ChoiceFour": "Lyla Lerrol", 
+"Answer": "Lana Lang", 
+"GIPHYID": "Lana+Lang+Superman"},
+
+{"Question": "Who did Superman entrust with a Kryptonite ring to stop him if he ever turned evil?",
+"ChoiceOne": "Wonder Woman", 
+"ChoiceTwo": "Lois Lane", 
+"ChoiceThree": "Batman", 
+"ChoiceFour": "Jimmy Olsen", 
+"Answer": "Batman", 
+"GIPHYID": "Superman+Batman"},
+
+{"Question": "Who created Superman?",
+"ChoiceOne": "Jerry Siegel & Stan Lee", 
+"ChoiceTwo": "Joe Shuster & Jerry Siegel", 
+"ChoiceThree": "Jack Kirby & Bob Kane", 
+"ChoiceFour": "Bob Kane & Joe Shuster", 
+"Answer": "Joe Shuster & Jerry Siegel", 
+"GIPHYID": "Superman"},
+
+{"Question": "What is Superman's Kryptonian name?",
+"ChoiceOne": "Jimbo", 
+"ChoiceTwo": "Jor-El", 
+"ChoiceThree": "Kal-El", 
+"ChoiceFour": "Zor-El", 
+"Answer": "Kal-El", 
+"GIPHYID": "Krypton+Superman"},
+
+{"Question": "What super-villain once broke Batman's back, leaving him crippled?",
+"ChoiceOne": "Bane", 
+"ChoiceTwo": "Joker", 
+"ChoiceThree": "Killer Croc", 
+"ChoiceFour": "Ra's Al Ghul", 
+"Answer": "Bane", 
+"GIPHYID": "Bane"},
+
+{"Question": "Which of these Bat-villains was introduced first?",
+"ChoiceOne": "The Riddler", 
+"ChoiceTwo": "Mr. Freeze", 
+"ChoiceThree": "Catwoman", 
+"ChoiceFour": "The Penguin", 
+"Answer": "Catwoman", 
+"GIPHYID": "Catwoman"},
+
+{"Question": "Which of the following characters did Bruce Wayne have a son with?",
+"ChoiceOne": "Catwoman", 
+"ChoiceTwo": "Vesper Fairchild", 
+"ChoiceThree": "Poison Ivy", 
+"ChoiceFour": "Talia Al Ghul", 
+"Answer": "Talia Al Ghul", 
+"GIPHYID": "Talia+Al+Ghul+Batman"},
+
+{"Question": "What superhero team did Batman found with Metamorpho and Black Lightning?",
+"ChoiceOne": "Crusaders", 
+"ChoiceTwo": "All-Star Squadron", 
+"ChoiceThree": "Outsiders", 
+"ChoiceFour": "Batman Incorporated", 
+"Answer": "Outsiders", 
+"GIPHYID": "Batman"},
+
+{"Question": "What former DA became the villain known as Two-Face?",
+"ChoiceOne": "Jason Blood", 
+"ChoiceTwo": "Edward Nygma", 
+"ChoiceThree": "Harvey Dent", 
+"ChoiceFour": "Floyd Lawton", 
+"Answer": "Harvey Dent", 
+"GIPHYID": "Two+Face+Batman"},
+
+{"Question": "What is Captain America's secret identity?",
+"ChoiceOne": "Tony Stark", 
+"ChoiceTwo": "Steve Rogers", 
+"ChoiceThree": "Peter Parker", 
+"ChoiceFour": "Clint Barton", 
+"Answer": "Steve Rogers", 
+"GIPHYID": "Captain+America"},
+
+{"Question": "Who was Captain America's sidekick during WW2?",
+"ChoiceOne": "Miss America",
+"ChoiceTwo": "Flag Boy", 
+"ChoiceThree": "Union Jack", 
+"ChoiceFour": "Bucky", 
+"Answer": "Bucky", 
+"GIPHYID": "Captain+America"},
+
+{"Question": "What was the original shape of Captain America's shield?",
+"ChoiceOne": "Star",
+"ChoiceTwo": "Badge", 
+"ChoiceThree": "Circular", 
+"ChoiceFour": "Square", 
+"Answer": "Badge", 
+"GIPHYID": "Captain+America+Shield"},
+
+{"Question": "What is the name of Thor's enchanted hammer?",
+"ChoiceOne": "Fenris",
+"ChoiceTwo": "Gungir", 
+"ChoiceThree": "Mjolnir", 
+"ChoiceFour": "Asgard", 
+"Answer": "Mjolnir", 
+"GIPHYID": "Thor+Hammer"},
+
+{"Question": "Who is Thor's father?",
+"ChoiceOne": "Balder",
+"ChoiceTwo": "Fenris", 
+"ChoiceThree": "Loki", 
+"ChoiceFour": "Odin", 
+"Answer": "Odin", 
+"GIPHYID": "Thor"},
+
+{"Question": "What animals pull Thor's chariot?",
+"ChoiceOne": "Wolves",
+"ChoiceTwo": "Lions", 
+"ChoiceThree": "Goats", 
+"ChoiceFour": "Bulls", 
+"Answer": "Goats", 
+"GIPHYID": "Thor+Chariot"},
+
+{"Question": "What actor plays Thor?",
+"ChoiceOne": "Mark Ruffalo",
+"ChoiceTwo": "Chris Hemsworth", 
+"ChoiceThree": "Chris Evans", 
+"ChoiceFour": "Jeremy Renner", 
+"Answer": "Chris Hemsworth", 
+"GIPHYID": "Chris+Hemsworth"},
+
+{"Question": "Which of Thor's enemies was a child of Loki?",
+"ChoiceOne": "Ymir",
+"ChoiceTwo": "Fenris Wolf", 
+"ChoiceThree": "Lorelei", 
+"ChoiceFour": "Mephisto", 
+"Answer": "Fenris Wolf", 
+"GIPHYID": "Loki"},
+
+{"Question": "Which of Wonder Woman's accessories was forged from the magic girdle of Aphrodite?",
+"ChoiceOne": "Her lasso",
+"ChoiceTwo": "Her bracelets", 
+"ChoiceThree": "Her tiara", 
+"ChoiceFour": "Her earrings", 
+"Answer": "Her lasso", 
+"GIPHYID": "Wonder+Woman+lasso"},
+
+{"Question": "Which of Wonder Woman's accessories allows her to breathe in outer space?",
+"ChoiceOne": "Her lasso",
+"ChoiceTwo": "Her bracelets", 
+"ChoiceThree": "Her tiara", 
+"ChoiceFour": "Her earrings", 
+"Answer": "Her earrings", 
+"GIPHYID": "Wonder+Woman"},
+
+{"Question": "What was the first superhero team Wonder Woman ever joined?",
+"ChoiceOne": "All Star squadron",
+"ChoiceTwo": "Justice Society of America", 
+"ChoiceThree": "Justice League of America", 
+"ChoiceFour": "Doom Patrol", 
+"Answer": "Justice Society of America", 
+"GIPHYID": "Wonder+Woman"}
+];
+
+var arrayOfQuotes = ["With great power comes great responsibility", "Why so serious?",
+"That's my secret Captain. I'm always angry!", "It's not who I am underneath, but what I do that defines me.",
+"There is a right and wrong in the Universe. And the distinction is not hard to make.", 
+"Cats come when they feel like, not when they're told.", "Some men aren't looking for something logical, some men just want to watch the world burn.",
+"No matter how bad things get, something good is out there, just over the horizon.", 
+"It doesn't matter who we are. What matters is our plan.", "Just because someone stumbles and loses their path, doesn't mean they can't be saved.",
+"There are no heroes and villains. There's just what I want and how I'll get it.",
+"If you could make God bleed, people would cease to believe in him.", "The answers you seek shall be yours, once I claim what is mine.",
+"I don't like bullies. I don't care where they're from.", "If you cage the beast, the beast will get angry.",
+"Freedom is life's great lie. Once you accept that in your heart, you will know peace.",
+"I shouldn't be alive, unless it was for a reason. I know what I have to do, and I know it is right.",
+"Life doesn't give us purpose. We give life purpose.", 
+"The strength of this country isn't in buildings of bricks and steel. It's in the hearts of those who have sworn to fight for its freedom.",
+"You're much stronger than you think you are. Trust me.", "In a world of ordinary mortals, you are a wonder woman.",
+"You only have your thoughts and dreams ahead of you. You are someone. You mean something.",
+"When you decide not to be afraid, you can find friends in super unexpected places."];
+
+var correct = 0;
+var wrong = 0;
+var unanswered = 0;
+var questionsAsked = 0;
+var questionsToAnswer = 10;
+var timeToAnswer = 25;
+var gifLimit = 5;
+var resultScreenShow = 5;
+var tickerLock = 1;
+
+function replaceOpeningScreen(){
+	$("#opening-screen").addClass("hidden");
+	$("#question-screen").removeClass("hidden");
+}
+
+function shuffleArray(arr){
+	for(var i = 1; i < arr.length; i++) {
+		var random = Math.floor(Math.random() * (i + 1));
+		if(random !== i) {
+			var dummy = arr[random];
+			arr[random] = arr[i];
+			arr[i] = dummy;
+		}
+	}
+	return arr;
+}
+
+function populateQA(){
+	questionsAsked++;
+	if(questionsAsked <= questionsToAnswer) {
+		$("#time").text(timeToAnswer);
+		tickerLock = 0;
+		var arrayOfChoices = [arrayOfQuestionObjects[questionsAsked].ChoiceOne, 
+		arrayOfQuestionObjects[questionsAsked].ChoiceTwo, arrayOfQuestionObjects[questionsAsked].ChoiceThree,
+		arrayOfQuestionObjects[questionsAsked].ChoiceFour];
+		arrayOfChoices = shuffleArray(arrayOfChoices);
+		$("#question").text(arrayOfQuestionObjects[questionsAsked].Question);
+		$("#choice-a").text(arrayOfChoices[0]);
+		$("#choice-b").text(arrayOfChoices[1]);
+		$("#choice-c").text(arrayOfChoices[2]);
+		$("#choice-d").text(arrayOfChoices[3]);
+		parseAnswer();
+	}
+	else {
+		$("#result-screen").addClass("hidden");
+		$("#final-statistics").removeClass("hidden");
+		if(correct >= 7)
+			$("#verdict").text("You are a bona fide comic book nerd!");
+		else if(correct <= 3)
+			$("#verdict").text("Well, at least you have a life.")
+		else
+			$("#verdict").text("You have a healthy knowledge of comics.");
+		$("#correct-tally").text(correct);
+		$("#wrong-tally").text(wrong);
+		$("#unanswered-tally").text(unanswered);
+		$(document).on("click", function(){
+			$(document).unbind("click");
+			correct = 0;
+			wrong = 0;
+			unanswered = 0;
+			questionsAsked = 0;
+			arrayOfQuestionObjects = shuffleArray(arrayOfQuestionObjects);
+			$("#final-statistics").addClass("hidden");
+			$("#question-screen").removeClass("hidden");
+			populateQA();
+		});
+	}
+}
+
+function countdown(){
+	if(tickerLock != 1) {
+		timeToAnswer--;
+		$("#time").text(timeToAnswer);
+		if(timeToAnswer === 0) {
+			timeToAnswer = 25;
+			tickerLock = 1;
+			openTimeUpScreen();
+		}
+	}
+}
+
+function parseAnswer(){
+	$(".answer-container").on("click", function() {
+		$(".answer-container").unbind("click");
+		tickerLock = 1;
+		timeToAnswer = 25;
+		var userChoice = $(this).attr("choice");
+		$("#question-screen").addClass("hidden");
+		$("#result-screen").removeClass("hidden")
+		if($("#choice-" + userChoice).text() === arrayOfQuestionObjects[questionsAsked].Answer) {
+			var random = Math.floor(Math.random() * arrayOfQuotes.length);
+			$("#result").text("Correct!");
+			$("#correct-answer").text(arrayOfQuotes[random]);
+			correct++;
+		}
+		else {
+			$("#result").text("Wrong!");
+			$("#correct-answer").text("Correct answer: " + arrayOfQuestionObjects[questionsAsked].Answer);
+			wrong++;
+		}
+		embedGIF();
+		setTimeout(function(){
+			if(questionsAsked < questionsToAnswer) {
+				$("#result-screen").addClass("hidden");
+				$("#question-screen").removeClass("hidden");
+			}
+			populateQA();}, 1000*resultScreenShow);
+	});
+}
+
+function embedGIF(){
+	$.ajax({
+		url: "http://api.giphy.com/v1/gifs/search?q=" + arrayOfQuestionObjects[questionsAsked].GIPHYID
+		+ "&api_key=61TKqzUDPHfv40Bqr6iEsqqBCfa360mt&limit=" + gifLimit,
+		method: "GET"
+	}).then(function(response) {
+		var random = Math.floor(Math.random()*gifLimit);
+		while(response.data[random].embed_url === "")
+			random = Math.floor(Math.random()*gifLimit);
+		var gifSelection = response.data[random];
+		$("#gif").html("<iframe src='" + gifSelection.embed_url + "' width='" + gifSelection.images["480w_still"]["width"] +
+			"' height='" + gifSelection.images["480w_still"]["height"] + "' alt='" + arrayOfQuestionObjects[questionsAsked].GIPHYID + 
+			"'>");
+	});
+}
+
+function openTimeUpScreen(){
+	$(".answer-container").unbind("click");
+	$("#question-screen").addClass("hidden");
+	$("#result-screen").removeClass("hidden");
+	$("#result").text("Time's Up!");
+	$("#correct-answer").text("Correct answer: " + arrayOfQuestionObjects[questionsAsked].Answer);
+	embedGIF();
+	unanswered++;
+	setTimeout(function(){
+		if(questionsAsked < questionsToAnswer) {
+			$("#result-screen").addClass("hidden");
+			$("#question-screen").removeClass("hidden");
+		}
+		populateQA();}, 1000*resultScreenShow);
+}
+
+$(document).ready(function(){
+	$(document).on("click", function(){
+		$(document).unbind("click");
+		arrayOfQuestionObjects = shuffleArray(arrayOfQuestionObjects);
+		replaceOpeningScreen();
+		setInterval(countdown, 1000);
+		populateQA();
+	});
+});
